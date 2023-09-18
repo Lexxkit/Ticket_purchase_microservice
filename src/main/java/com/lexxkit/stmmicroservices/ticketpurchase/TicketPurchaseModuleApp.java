d7e9fbb5a85b91b4f8dab648d73e6ece9a5d072c
Lexxkit <lexxkit@gmail.com>
2023-09-18 10:57:38 +0100

    Initial commit

---
+++ src/main/java/com/lexxkit/stmmicroservices/ticketpurchase/TicketPurchaseModuleApp.java
@@ -0,0 +1,13 @@
+package com.lexxkit.stmmicroservices.ticketpurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketPurchaseModuleApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketPurchaseModuleApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/lexxkit/stmmicroservices/ticketpurchase/TicketPurchaseModuleAppTests.java
@@ -0,0 +1,13 @@
+package com.lexxkit.stmmicroservices.ticketpurchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPurchaseModuleAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
