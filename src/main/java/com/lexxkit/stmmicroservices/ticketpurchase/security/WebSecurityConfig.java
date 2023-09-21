ad6bc476ab313b36b6aa9aeb102367eda774bb39
Lexxkit <lexxkit@gmail.com>
2023-09-21 12:34:19 +0100

    Adjusted Spring Security configs for Basic security;

--- src/main/java/com/lexxkit/stmmicroservices/ticketpurchase/security/WebSecurityConfig.java
+++ src/main/java/com/lexxkit/stmmicroservices/ticketpurchase/security/WebSecurityConfig.java
@@ -2,25 +2,25 @@ package com.lexxkit.stmmicroservices.ticketpurchase.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity()
+@EnableMethodSecurity
 public class WebSecurityConfig {
 
   private static final String[] AUTH_WHITELIST = {
-      "/swagger-resources/**",
       "/swagger-ui/**",
       "/swagger-ui/index.html",
-      "/api/**",
-      "/v3/api-docs",
+      "/v3/api-docs/**",
       "/webjars/**",
       "/api/users/login",
       "/api/users/register"
@@ -29,12 +29,14 @@ public class WebSecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
+        .httpBasic(Customizer.withDefaults())
+        .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests((authz) ->
         authz
             .requestMatchers(AUTH_WHITELIST).permitAll()
-            .requestMatchers("/api/tickets/**").authenticated())
-        .cors(Customizer.withDefaults())
-        .httpBasic(Customizer.withDefaults());
+            .requestMatchers(HttpMethod.POST, "/api/tickets/**").authenticated()
+            .requestMatchers(HttpMethod.GET, "/api/tickets/**").permitAll()
+        );
 
     return http.build();
   }
