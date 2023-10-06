7ad869d93558e16389e0ff1794cfb789ff3a1d81
Lexxkit <lexxkit@gmail.com>
2023-10-06 11:53:46 +0100

    Added ENV VARIABLES for JWT properties to .env; refactor application.properties with ENV VARS;

--- src/main/java/com/lexxkit/stmmicroservices/ticketpurchase/exception/RestRespExceptionHandler.java
+++ src/main/java/com/lexxkit/stmmicroservices/ticketpurchase/exception/RestRespExceptionHandler.java
@@ -21,7 +21,17 @@ public class RestRespExceptionHandler {
     );
   }
 
-  record ErrorMessage(int statusCode, String message, String description) {
+  @ExceptionHandler(value = AuthenticationException.class)
+  @ResponseStatus(HttpStatus.FORBIDDEN)
+  public ErrorMessage handleAuthenticationException(RuntimeException ex, WebRequest req) {
+    return new ErrorMessage(
+        HttpStatus.FORBIDDEN.value(),
+        ex.getMessage(),
+        req.getDescription(false)
+    );
+  }
+
+  public record ErrorMessage(int statusCode, String message, String description) {
 
   }
 }
--- src/main/java/com/lexxkit/stmmicroservices/ticketpurchase/security/jwt/JwtProvider.java
+++ src/main/java/com/lexxkit/stmmicroservices/ticketpurchase/security/jwt/JwtProvider.java
@@ -83,15 +83,15 @@ public class JwtProvider {
           .parseClaimsJws(token);
       return true;
     } catch (ExpiredJwtException expEx) {
-      log.error("Token expired", expEx);
+      log.error("Token expired: {}", expEx.getMessage());
     } catch (UnsupportedJwtException unsEx) {
-      log.error("Unsupported jwt", unsEx);
+      log.error("Unsupported jwt: {}", unsEx.getMessage());
     } catch (MalformedJwtException mjEx) {
-      log.error("Malformed jwt", mjEx);
+      log.error("Malformed jwt: {}", mjEx.getMessage());
     } catch (SignatureException sEx) {
-      log.error("Invalid signature", sEx);
-    } catch (Exception e) {
-      log.error("invalid token", e);
+      log.error("Invalid signature: {}", sEx.getMessage());
+    } catch (IllegalArgumentException e) {
+      log.error("Invalid token: {}", e.getMessage());
     }
     return false;
   }
